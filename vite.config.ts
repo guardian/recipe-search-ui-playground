bc93ecc69e70facc6036b07e5d652ccdc1435f7f
Andy Gallagher <fredex42@gmail.com>
2024-08-23 21:54:44 +0100

    no tests yet!

--- vite.config.ts
+++ vite.config.ts
@@ -38,6 +38,7 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    passWithNoTests: true,
 
     reporters: ['default'],
     coverage: {
