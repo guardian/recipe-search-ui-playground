7c5278b8f0963dddd78965c2399651a724c43dee
Andy Gallagher <fredex42@gmail.com>
2024-09-02 16:55:06 +0100

    add "show more results"

--- src/app/ResultsList.tsx
+++ src/app/ResultsList.tsx
@@ -1,6 +1,10 @@
 import { TitleSearchResult } from '../service/schema';
 import { sideScollingListItem, sideScrollingList } from './ListStyles';
 import { RecipeCard } from './RecipeCard';
+import { useEffect, useState } from 'react';
+import { css, Typography } from '@mui/material';
+import { ResultCard } from './ResultCard';
+import { More, ReadMore } from '@mui/icons-material';
 
 interface ResultsListProps {
   results: TitleSearchResult[];
@@ -8,14 +12,37 @@ interface ResultsListProps {
   scoreCutoff?: number;
 }
 
+const clickable = css`
+    cursor: pointer;
+    height: 100%;
+`;
+
 export const ResultsList = ({results, showScore, scoreCutoff}:ResultsListProps) => {
-  const resultsToRender = scoreCutoff ? results.filter((r)=>r.score>scoreCutoff) : results;
+  const [showLessRelevant, setShowLessRelevant] = useState(false);
+  const [haveLessRelevant, setHaveLessRelevant] = useState(false);
+
+  const resultsToRender = scoreCutoff && !showLessRelevant ? results.filter((r)=>r.score>scoreCutoff) : results;
+
+  useEffect(() => {
+    const lastResult = results[results.length - 1];
+    setHaveLessRelevant(lastResult.score <= (scoreCutoff ?? 0));
+    setShowLessRelevant(false);
+  }, [results, scoreCutoff]);
+
+  const showAll = ()=>setShowLessRelevant(true);
 
   return <ul css={sideScrollingList}>
     {
       resultsToRender.map(((hit, ctr)=><li css={sideScollingListItem} key={ctr}>
         <RecipeCard {...hit} showScore={showScore} onClick={()=>{ } }/>
       </li>))
+    }
+    {
+      haveLessRelevant && !showLessRelevant ? <li css={sideScollingListItem}>
+        <div css={clickable} onClick={showAll}>
+          <ResultCard onClick={showAll} title="Show less relevant..." icon={<ReadMore/>}/>
+        </div>
+      </li>: undefined
     }
   </ul>
 }
